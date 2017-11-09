8dbe5d48acd50411f2a547c0e4953ad16b9e1dc8
mandemeskel <mandemeskel@users.noreply.github.com>
2017-11-09 11:48:51 -0800

    lil house cleaning

--- app/src/main/java/io/mta/apo/MainActivity.kt
+++ app/src/main/java/io/mta/apo/MainActivity.kt
@@ -3,7 +3,6 @@ package io.mta.apo
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.SurfaceView
 import android.view.View
 import android.widget.ImageButton
 
