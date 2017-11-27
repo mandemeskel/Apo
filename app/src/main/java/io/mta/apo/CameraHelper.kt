3fdc982ca7728aecb4f42560fa9d09369cbfba9d
mandemeskel <mandemeskel@users.noreply.github.com>
2017-11-26 19:02:50 -0800

    updated camera helper to use CameraView instead of native camera api

--- app/src/main/java/io/mta/apo/CameraHelper.kt
+++ app/src/main/java/io/mta/apo/CameraHelper.kt
@@ -2,18 +2,16 @@ package io.mta.apo
 
 import android.content.Context
 import android.content.pm.PackageManager
-import android.view.Surface
 import android.Manifest
-import android.hardware.camera2.*
+import android.util.Log
+import com.otaliastudios.cameraview.CameraListener
+import com.otaliastudios.cameraview.CameraView
 
 /**
  * Created by Michael T. Andemeskel on 11/12/17.
  */
-class CameraHelper (val context: Context, val surfaces: List<Surface>) {
-    var camera_manager: CameraManager? = null
-    var main_camera_id: String = ""
-    var camera_callback: CameraCallback? = null
-    val session_callback: CameraSessionCallback = CameraSessionCallback()
+class CameraHelper (val context: Context, val camera: CameraView) {
+    var camera_callback: CameraListener? = null
 
     init {
 
@@ -23,12 +21,10 @@ class CameraHelper (val context: Context, val surfaces: List<Surface>) {
         if(this.isCameraAvailable() == false)
             throw Exception("No available camera")
 
-        camera_manager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        camera_callback = CaptureListener()
+        camera.addCameraListener(camera_callback)
 
-        main_camera_id = this.getBackCamera()
-
-        camera_callback = CameraCallback(surfaces, session_callback as CameraCaptureSession.StateCallback)
-
+        cameraStart()
     }
 
     /**
@@ -46,80 +42,34 @@ class CameraHelper (val context: Context, val surfaces: List<Surface>) {
     }
 
     /**
-     * Get the back facing camera we will use to take pictures with
+     * Camera capture listener.
      */
-    fun getBackCamera(): String {
-        // go through list of cameras on the phone
-        val cameras: Array<String> = camera_manager!!.cameraIdList
-        for(camera in cameras){
-
-            // get the details of the camera
-            val stats: CameraCharacteristics = (camera_manager as CameraManager).getCameraCharacteristics(camera)
-
-            // find a the camera on the back of the phone
-            if(stats[CameraCharacteristics.LENS_FACING] == CameraMetadata.LENS_FACING_BACK)
-                return camera
-
-        }
-
-        return ""
-    }
-
-    /**
-     * Let's us connect to the main camera.
-     */
-    fun openCamera() {
-        if(checkForPermission() == false)
-            throw Exception("Don't have camera permission");
-
-        // https://developer.android.com/reference/android/hardware/camera2/CameraManager.html#openCamera(java.lang.String, android.hardware.camera2.CameraDevice.StateCallback, android.os.Handler)
-        camera_manager?.openCamera(
-            main_camera_id,
-            // https://developer.android.com/reference/android/hardware/camera2/CameraCaptureSession.html#CameraCaptureSession()
-            camera_callback,
-            // https://developer.android.com/reference/android/os/Handler.html
-            null
-        )
-    }
-
-    // https://developer.android.com/reference/android/hardware/camera2/CameraDevice.StateCallback.html
-    /**
-     * Handles camera state changes, this where we get the camera device
-     */
-    class CameraCallback(val surfaces: List<Surface>, val capture_session_callback: CameraCaptureSession.StateCallback): CameraDevice.StateCallback() {
-
-        override fun onDisconnected(camera: CameraDevice?) {
-            TODO("not implemented")
-        }
-
-        override fun onError(camera: CameraDevice?, p1: Int) {
-            TODO("not implemented")
-        }
-
-        override fun onOpened(camera: CameraDevice?) {
-            camera?.createCaptureSession(
-                surfaces,
-                capture_session_callback,
-                null
-            )
-        }
-
-    }
-
-    class CameraSessionCallback: CameraCaptureSession.StateCallback() {
-        override fun onConfigureFailed(session: CameraCaptureSession?) {
-
-        }
-
-        override fun onConfigured(session: CameraCaptureSession?) {
-
+    class CaptureListener: CameraListener() {
+        override fun onPictureTaken(jpeg: ByteArray?) {
+            super.onPictureTaken(jpeg)
+            // TODO: do something with the picture
+            Log.i("CaptureListener:", "picture taken, byte array size: " + jpeg?.size.toString())
         }
     }
 
     /**
-     * Capture an image.
+     * Get access to the camera resources.
      */
-    fun capture(surface: Surface) {
+    fun cameraStart() {
+        camera.start()
+    }
 
+    /**
+     * Stop listening to camera resources.
+     */
+    fun cameraStop() {
+        camera.stop()
+    }
+
+    /**
+     * Release camera resources.
+     */
+    fun cameraDestroy() {
+        camera.destroy()
     }
 }
