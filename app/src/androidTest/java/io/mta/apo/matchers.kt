9652d27285fd568901fbead50c78b7a69c85035f
mandemeskel <mandemeskel@users.noreply.github.com>
2017-12-02 16:05:47 -0800

    replaced matcher class with function, added test but alas it was all for naught as ColorFilter can not be instatiated due to a deprecated construcotr

--- app/src/androidTest/java/io/mta/apo/matchers.kt
+++ app/src/androidTest/java/io/mta/apo/matchers.kt
@@ -1,19 +1,24 @@
 package io.mta.apo
 
+import android.graphics.ColorFilter
 import android.support.test.espresso.matcher.BoundedMatcher
 import android.view.View
 import android.widget.ImageButton
 import org.hamcrest.Description
+import org.hamcrest.Matcher
+
 
 /**
  * Created by Michael T. Andemeskel on 12/2/17.
  */
-class withFilterColor: BoundedMatcher<View, ImageButton>(ImageButton::class.java) {
-    override fun matchesSafely(item: ImageButton?): Boolean {
-        // TODO
-    }
+fun withColorFilter(color: ColorFilter): Matcher<View> {
+    return object: BoundedMatcher<View, ImageButton>(ImageButton::class.java) {
+        public override fun matchesSafely(img: ImageButton?): Boolean {
+            return color == img!!.colorFilter
+        }
 
-    override fun describeTo(description: Description?) {
-        // TODO
+        override fun describeTo(description: Description?) {
+            description!!.appendText("with color filter: " + color.toString())
+        }
     }
 }
