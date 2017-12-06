b2cd1f1493b543f7514aec9c855c99d4afc8163e
mandemeskel <mandemeskel@users.noreply.github.com>
2017-12-05 21:07:07 -0800

    created pills class to hold pill info and properties

---
+++ app/src/main/java/io/mta/apo/Pill.kt
@@ -0,0 +1,33 @@
+package io.mta.apo
+
+/**
+ * Created by Michael T. Andemeskel on 12/5/17.
+ */
+class Pill (val brand_name: String, val medical_name: String, val img_path: String, val description: String) {
+
+    init {
+        // TODO
+    }
+
+    /**
+     * Saves pill to recent searches list.
+     */
+    fun savePill() {
+        // TODO
+    }
+
+    // companion object to host static functions and properties
+    companion object Handler {
+
+        /**
+         * Loads saved pills from recent searches list
+         */
+        fun loadSavedPills(): Array<Pill>? {
+            // TODO
+            var pills: Array<Pill>? = null
+            return pills
+        }
+
+
+    }
+}
