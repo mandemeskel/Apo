352e21a350797a7ee52e69abe0c4774e1a4bbb83
Michael Andemeskel <mandemeskel@ucmerced.edu>
2017-12-07 21:43:59 -0800

    added toolbar to recent searches page

--- app/src/main/java/io/mta/apo/RecentSearchesActivity.kt
+++ app/src/main/java/io/mta/apo/RecentSearchesActivity.kt
@@ -2,11 +2,16 @@ package io.mta.apo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class RecentSearchesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recent_searchs)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
 }
