17360f9b8e3dfe95ae47c29079c1a6f962ebcdb3
mandemeskel <mandemeskel@users.noreply.github.com>
2017-12-10 20:51:00 -0800

    added database class, implemented pill entity inside of it

---
+++ app/src/main/java/io/mta/apo/AppDatabase.kt
@@ -0,0 +1,17 @@
+package io.mta.apo
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+
+/**
+ * Created by Michael T. Andemeskel on 12/10/17.
+ */
+/**
+ * A database class, this class is how we interact with the database and get a connection to it.
+ *
+ * @property pillDao the pill database accessor object so we can query the pill table
+ */
+@Database(entities = [Pill.PillEntity::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun pillDao(): Pill.PillDao
+}
