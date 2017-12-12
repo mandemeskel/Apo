b2517fc7242fcde36ef1d20dc29e27c91e61f89d
mandemeskel <mandemeskel@users.noreply.github.com>
2017-12-12 08:45:29 -0800

    DateConverter needs to be instantiated, it needs to be a class

--- app/src/androidTest/java/io/mta/apo/PillTests.kt
+++ app/src/androidTest/java/io/mta/apo/PillTests.kt
@@ -3,6 +3,7 @@ package io.mta.apo
 import android.support.test.InstrumentationRegistry
 import android.support.test.filters.LargeTest
 import android.support.test.runner.AndroidJUnit4
+import android.util.Log
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Test
@@ -15,11 +16,13 @@ import org.junit.runner.RunWith
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class PillTests {
+    val TAG = "PillTests"
+    val pill_id = 209
     val pill_brand_name = "advil"
     val pill_medical_name = "ibuprofem"
     val pill_img_path = "img/path/img.jpg"
     val pill_description = "wonder drug"
-    val test_pill = Pill(pill_brand_name, pill_medical_name, pill_img_path, pill_description)
+    val test_pill = Pill(pill_id, pill_brand_name, pill_medical_name, pill_img_path, pill_description)
     val context = InstrumentationRegistry.getTargetContext()
 
     @Before
@@ -34,15 +37,18 @@ class PillTests {
 
     @Test
     fun pill_can_be_saved_and_loaded() {
+        test_pill.save()
+        Pill.savePills(context)
         val saved_pills = Pill.loadSavedPills(context)
-        assertThat("loadSavedPills returns a list of pills that contains saved pill.", saved_pills.contains(test_pill.entity))
+        val found = saved_pills.filter { pill -> pill.id == test_pill.entity.id }
+        assertThat("loadSavedPills returns a list of pills that contains saved pill.", found.size == 1)
     }
 
     @Test
     fun saved_pills_can_be_cleared() {
         Pill.clearSavedPills(context)
         val saved_pills = Pill.loadSavedPills(context)
-        assert(saved_pills.isEmpty() == true)
+        assertThat("clearSavedPills clears the pills table", saved_pills.isEmpty() == true)
     }
 
 }
