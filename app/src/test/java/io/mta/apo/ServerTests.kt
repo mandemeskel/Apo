808794193ba8f684b7c0fb208d746f9f520ab2b9
mandemeskel <mandemeskel@users.noreply.github.com>
2017-12-13 02:37:48 -0800

    updated test to utf8 urlencode query parameters

--- app/src/test/java/io/mta/apo/ServerTests.kt
+++ app/src/test/java/io/mta/apo/ServerTests.kt
@@ -2,22 +2,31 @@ package io.mta.apo
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
+import java.net.URLEncoder
 
-class ServerTests {
-    val pill_name = "Advil"
-    val pill_imprint = "A"
-    val pill_color = "white"
-    val server = Server()
+class ServerTests {    val server = Server()
+
+    val TAG = "ServerTests"
+    val pill_name = "Advil PM"
+    val pill_imprint = "Advil"
+    val pill_color = "BLUE"
+    val utf8_name = URLEncoder.encode(pill_name, server.UTF8)
+    val utf8_imprint = URLEncoder.encode(pill_imprint, server.UTF8)
+    val utf8_color = URLEncoder.encode(pill_color, server.UTF8)
 
     @Test
     fun getPillSearchUrl() {
-        val expected_url = server.getPillEndpoint() + server.MEDICINE_NAME + pill_name + server.IMPRINT + pill_imprint + server.COLOR + pill_color
+        val expected_url = server.getPillEndpoint() + server.MEDICINE_NAME + utf8_name + server.IMPRINT + utf8_imprint + server.COLOR + utf8_color
+
         val url = server.getPillSearchUrl(pill_name, pill_imprint, pill_color)
         assertThat("getPillSearchUrl should return a properly formated url", expected_url == url)
     }
 
     @Test
     fun queryServerPillOption() {
-        // TODO: should return list of pills
+        var url = server.getPillSearchUrl(pill_name, "", "")
+        System.out.println(url)
+        val data = server.queryServerPillOption(pill_name, "", "")
+        assertThat("queryServerPillOption returns a JSON string", data is String)
     }
 }
