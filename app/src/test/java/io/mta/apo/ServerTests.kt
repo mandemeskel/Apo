bb9df8e5e94701f441879eefb9da493db7ac733f
mandemeskel <mandemeskel@users.noreply.github.com>
2017-12-13 06:10:36 -0800

    added new test to check the quality of the pills returned by the server

--- app/src/test/java/io/mta/apo/ServerTests.kt
+++ app/src/test/java/io/mta/apo/ServerTests.kt
@@ -3,16 +3,20 @@ package io.mta.apo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import java.net.URLEncoder
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-class ServerTests {    val server = Server()
+class ServerTests {
 
     val TAG = "ServerTests"
+    val server = Server()
     val pill_name = "Advil PM"
     val pill_imprint = "Advil"
     val pill_color = "BLUE"
     val utf8_name = URLEncoder.encode(pill_name, server.UTF8)
     val utf8_imprint = URLEncoder.encode(pill_imprint, server.UTF8)
     val utf8_color = URLEncoder.encode(pill_color, server.UTF8)
+    var client:OkHttpClient = OkHttpClient()
 
     @Test
     fun getPillSearchUrl() {
@@ -23,8 +27,42 @@ class ServerTests {    val server = Server()
     }
 
     @Test
-    fun queryServerPillOption() {
-        val data = server.queryServerPillOption(pill_name, "", "")
+    fun queryServerPillOptionReturnsPillArray() {
+        val data = server.queryServerPillOption(pill_name)
         assertThat("queryServerPillOption should return an array of pills", data is Array<Pill>)
+    }
+
+    @Test
+    fun queryServerPillOptionReturnsAllPills() {
+        val url = server.getPillSearchUrl(pill_name)
+        val request = Request.Builder().url(url).build()
+        val response = client.newCall(request).execute()
+        val json = response.body()!!.string()
+        val num_objects = json.split("},{").size
+
+        val data = server.queryServerPillOption(pill_name)
+        assertThat("queryServerPillOption should return an array of all the pills returned by the query", data.size == num_objects)
+    }
+
+
+    @Test
+    fun queryServerPillOptionReturnsInitializedPills() {
+        val url = server.getPillSearchUrl(pill_name)
+        val request = Request.Builder().url(url).build()
+        val response = client.newCall(request).execute()
+        val json = response.body()!!.string()
+
+        val pills = server.queryServerPillOption(pill_name)
+        // I chose to test these particular fields because all pills in the
+        // database have a value for these fields
+        for(pill in pills) {
+            assertThat("queryServerPillOption should return pills with ids", json.contains(pill.id.toString()))
+
+            assertThat("queryServerPillOption should return pills with brand names", pill.brand_name.isNotBlank())
+
+            assertThat("queryServerPillOption should return pills with medical names", pill.medical_name.isNotBlank())
+
+            assertThat("queryServerPillOption should return pills with descriptions", pill.description.isNotBlank())
+        }
     }
 }
